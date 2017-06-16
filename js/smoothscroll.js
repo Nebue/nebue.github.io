a36b7a9496ad8a50502eb16b4ae59123427574d8
MSI\Nebue Yemane <99039746@mydavinci.nl>
2017-06-16 09:53:45 +0200

    Nieuwe Website

---
+++ js/smoothscroll.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+jQuery(document).ready(function() {
+    var offset = 220;
+    var duration = 800;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > offset) {
+            jQuery('.back-to-top').fadeIn(duration);
+        } else {
+            jQuery('.back-to-top').fadeOut(duration);
+        }
+    });
+    
+    jQuery('.back-to-top').click(function(event) {
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
+});
+
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if($(document).scrollTop() > 10) {
+      $('#nav').addClass('shrink');
+    }
+    else {
+    $('#nav').removeClass('shrink');
+    }
+  });
+});
+
+
+
